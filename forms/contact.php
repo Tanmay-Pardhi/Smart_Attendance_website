acfc2bed795c176b6b546fbd8c69f80703bd945e
Tanmay Pardhi <55151822+Tanmay-Pardhi@users.noreply.github.com>
2021-05-05 12:23:14 +0530

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,9 @@
+<?php
+ // Check if the form is submitted
+ if (isset(  $_POST['submit'] ))
+ {
+	 $personName =  $_POST['name'];
+	 $email =  $_POST['email'];
+	 $subject = $_POST['subject'];
+ }
+?>
